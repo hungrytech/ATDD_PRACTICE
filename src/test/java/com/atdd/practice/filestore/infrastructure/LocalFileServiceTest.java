07e5be8dd9e048022afcfb60c9dc58c8ace9339b
hungrytech <xorals9448@gmail.com>
2022-04-10 19:28:39 +0900

    test : 동영상 업로드 테스트 추가

--- src/test/java/com/atdd/practice/filestore/infrastructure/LocalFileServiceTest.java
+++ src/test/java/com/atdd/practice/filestore/infrastructure/LocalFileServiceTest.java
@@ -1,9 +1,9 @@
 package com.atdd.practice.filestore.infrastructure;
 
 import com.atdd.practice.common.config.ServiceTestForFile;
-import com.atdd.practice.common.fixture.MemberLoginInfoFixture;
 import com.atdd.practice.filestore.application.exception.InvalidFileExtensionException;
 import com.atdd.practice.filestore.application.exception.InvalidFileRequestException;
+import com.atdd.practice.filestore.application.exception.InvalidVideoFileExtensionException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -52,7 +52,7 @@ public class LocalFileServiceTest extends ServiceTestForFile {
         );
     }
 
-    @DisplayName("저장할 수 있는 확장자만 받을 수 있다.")
+    @DisplayName("저장할 수 있는 이미지 확장자만 받을 수 있다.")
     @Test
     void 이미지_업로드_실패_잘못된_확장자() throws Exception {
         // given
@@ -61,8 +61,6 @@ public class LocalFileServiceTest extends ServiceTestForFile {
                 INVALID_EXTENSION_IMAGE_FILE.getName(),
                 MediaType.MULTIPART_FORM_DATA_VALUE,
                 new FileInputStream(INVALID_EXTENSION_IMAGE_FILE));
-
-        String destination = System.getProperty("user.dir") + "/src/test/resources/file/test/";
 
         // when then
         assertThatThrownBy(() -> localFileService.uploadFile(mockMultipartFile, FileType.IMAGE, TEST_MEMBER_LOGIN_INFO.getEmail()))
@@ -74,5 +72,43 @@ public class LocalFileServiceTest extends ServiceTestForFile {
     void 파일_업로드_실패_파일_객체가_null() {
         assertThatThrownBy(() -> localFileService.uploadFile(null, FileType.IMAGE, TEST_MEMBER_LOGIN_INFO.getEmail()))
                 .isInstanceOf(InvalidFileRequestException.class);
+    }
+
+    @DisplayName("동영상 파일 업로드에 성공한다.")
+    @Test
+    void 동영상_업로드_성공() throws IOException {
+        // given
+        MultipartFile mockMultipartFile = new MockMultipartFile(
+                MP4_VIDEO_FILE.getName(),
+                MP4_VIDEO_FILE.getName(),
+                MediaType.MULTIPART_FORM_DATA_VALUE,
+                new FileInputStream(MP4_VIDEO_FILE));
+
+        // when
+        FileUploadResult filUploadResult = localFileService.uploadFile(
+                mockMultipartFile,
+                FileType.VIDEO,
+                TEST_MEMBER_LOGIN_INFO.getEmail());
+
+        // then
+        assertAll(
+                () -> assertThat(filUploadResult.getPath()).isNotNull(),
+                () -> assertThat(filUploadResult.getOriginalFileName()).isEqualTo(mockMultipartFile.getOriginalFilename())
+        );
+    }
+
+    @DisplayName("저장할 수 있는 동영상 확장자만 받을 수 있다.")
+    @Test
+    void 동영상_업로드_실패_잘못된_확장자() throws Exception {
+        // given
+        MultipartFile mockMultipartFile = new MockMultipartFile(
+                INVALID_EXTENSION_VIDEO_FILE.getName(),
+                INVALID_EXTENSION_VIDEO_FILE.getName(),
+                MediaType.MULTIPART_FORM_DATA_VALUE,
+                new FileInputStream(INVALID_EXTENSION_VIDEO_FILE));
+
+        // when then
+        assertThatThrownBy(() -> localFileService.uploadFile(mockMultipartFile, FileType.VIDEO, TEST_MEMBER_LOGIN_INFO.getEmail()))
+                .isInstanceOf(InvalidVideoFileExtensionException.class);
     }
 }
